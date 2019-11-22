e063a5fe1f8e6bc60bf4bc6916a478738ea0abe9
zcmich <zafackmichel@yahoo.fr>
2019-11-22 14:37:06 +0100

    not able to retrieve query from url

--- src/main/java/com/mySquire/controllers/endpointController.java
+++ src/main/java/com/mySquire/controllers/endpointController.java
@@ -3,25 +3,77 @@ package com.mySquire.controllers;
 import com.mySquire.services.DataSetUrlService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
 @RestController
-@RequestMapping("/squire")
+@RequestMapping("/squire/sparql-endpoint")
 public class endpointController {
     @Autowired
     DataSetUrlService service;
 
-    @GetMapping("/sparql-endpoint")
-    public String getDatasetUrl() throws UnsupportedEncodingException {
+    @GetMapping("/select/{query}")
+    public String select(@PathVariable(value = "query") String query) throws UnsupportedEncodingException {
+        return service.endpointDataUrlService(query);
+    }
 
-return service.getDatasetUrl();
+
+    @GetMapping("/ask/{query}")
+    public String ask(@PathVariable(value = "query") String query) throws UnsupportedEncodingException {
+        return service.endpointDataUrlService(query);
+    }
+
 
 }
-}
+
+
+
+
+
+//    private Client client = ClientBuilder.newClient();
+
+//    public String getDatasetUrl() throws UnsupportedEncodingException {
+//        //String params = URLEncoder.encode("SELECT DISTINCT ?s ?p ?o WHERE {?s ?p ?o.} LIMIT 1", "UTF-8");
+//        String params = URLEncoder.encode("SELECT DISTINCT ?s ?p ?o WHERE {?s ?p ?o.} LIMIT 1", "UTF-8");
+//        //System.out.println(params);
+//        String REST_URI = "http://opendatacommunities.org/sparql.json?query="+ params;
+//        return client
+//                .target(REST_URI)
+////                .path(String.valueOf(id))
+//                .request(MediaType.APPLICATION_JSON)
+//                .get(String.class);
+//    }
+
+
+
+
+
+
+//}
+
+//    SELECT DISTINCT ?s ?p ?o WHERE {?s ?p ?o.}?
+
+
+
+//    private Client client = ClientBuilder.newClient();
+//
+//    public String getDatasetUrl() throws UnsupportedEncodingException {
+//        String params = URLEncoder.encode("SELECT DISTINCT ?s ?p ?o WHERE {?s ?p ?o.} LIMIT 1", "UTF-8");
+//        String REST_URI = "http://opendatacommunities.org/sparql.json?query=" + params;
+//        return client
+//                .target(REST_URI)
+////                .path(String.valueOf(id))
+//                .request(MediaType.APPLICATION_JSON)
+//                .get(String.class);
+//    }
+
--- src/main/java/com/mySquire/services/DataSetUrlService.java
+++ src/main/java/com/mySquire/services/DataSetUrlService.java
@@ -13,12 +13,12 @@ import java.net.URLEncoder;
 public class DataSetUrlService {
     @Autowired
     DataSetUrlService service;
-
-
     private Client client = ClientBuilder.newClient();
 
-    public String getDatasetUrl() throws UnsupportedEncodingException {
-        String params = URLEncoder.encode("SELECT DISTINCT ?s ?p ?o WHERE {?s ?p ?o.} LIMIT 1", "UTF-8");
+    //pour le moment je n'est pas encore cree le service pour ASK.elle sera indentique a celle si .si on parvien a prend la requete en parametre
+    public String endpointDataUrlService(String query) throws UnsupportedEncodingException {
+//        String params = URLEncoder.encode("SELECT DISTINCT ?s ?p ?o WHERE {?s ?p ?o.} LIMIT 1", "UTF-8");
+        String params = URLEncoder.encode(query, "UTF-8");
         String REST_URI = "http://opendatacommunities.org/sparql.json?query=" + params;
         return client
                 .target(REST_URI)
